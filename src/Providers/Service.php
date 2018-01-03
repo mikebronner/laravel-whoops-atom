dec09d7dc22f08ae7fa56a2ad5b7f6d1cfae44ab
Mike Bronner <mike@genealabs.com>
2018-01-02 16:06:07 -0800

    Prevent package from being loaded in production

--- src/Providers/Service.php
+++ src/Providers/Service.php
@@ -5,42 +5,33 @@ use Illuminate\Support\ServiceProvider;
 
 class Service extends ServiceProvider
 {
-    protected $defer = false;
-
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../../config/genealabs-laravel-whoops-atom.php'
-                => config_path('genealabs-laravel-whoops-atom.php')
-        ], 'config');
+        if (config('app.env') !== 'production') {
+            $this->publishes([
+                __DIR__ . '/../../config/genealabs-laravel-whoops-atom.php'
+                    => config_path('genealabs-laravel-whoops-atom.php')
+            ], 'config');
 
-        config([
-            'app.editor' => function ($file, $line) {
-                $homestead = rtrim(config('genealabs-laravel-whoops-atom.homestead-sites-path'), '/');
-                $local = rtrim(config('genealabs-laravel-whoops-atom.local-sites-path'), '/');
+            config([
+                'app.editor' => function ($file, $line) {
+                    $homestead = rtrim(config('genealabs-laravel-whoops-atom.homestead-sites-path'), '/');
+                    $local = rtrim(config('genealabs-laravel-whoops-atom.local-sites-path'), '/');
 
-                if (! $local) {
-                    return '';
-                }
+                    if (! $local) {
+                        return '';
+                    }
 
-                $file = str_replace("{$homestead}/", "{$local}/", $file);
+                    $file = str_replace("{$homestead}/", "{$local}/", $file);
 
-                return "atom://open?url=file://{$file}&line={$line}";
-            },
-        ]);
-    }
-
-    public function register()
-    {
-        $this->commands(Publish::class);
-        $this->mergeConfigFrom(
-            __DIR__ . '/../../config/genealabs-laravel-whoops-atom.php',
-            'genealabs-laravel-whoops-atom'
-        );
-    }
-
-    public function provides() : array
-    {
-        return ['genealabs-laravel-whoops-atom'];
+                    return "atom://open?url=file://{$file}&line={$line}";
+                },
+            ]);
+            $this->commands(Publish::class);
+            $this->mergeConfigFrom(
+                __DIR__ . '/../../config/genealabs-laravel-whoops-atom.php',
+                'genealabs-laravel-whoops-atom'
+            );
+        }
     }
 }
